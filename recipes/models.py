9c0cebe597014c5ad64bf4431500d11275a15048
Emidio Valeretto <emidio.valereto@gmail.com>
2024-10-04 12:07:56 +0100

    chore: add some fields to Recipe class

--- recipes/models.py
+++ recipes/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.category_name
-    
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=100, verbose_name='Title')
     description = models.CharField(max_length=165)
     slug = models.SlugField(unique=True)
+    ingredients = models.TextField()
+    is_ingredients_a_html = models.BooleanField(default=False)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
@@ -27,15 +27,13 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-                Category, 
-                on_delete=models.SET_NULL, 
-                null=True, 
-                blank=True, 
-                default=None
-            )
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None
+    )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
 
     def __str__(self):
         return self.title
-    

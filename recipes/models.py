69a1963ae62f060d99f1a721693f38441620ae7c
Emidio Valeretto <emidio.valereto@gmail.com>
2024-10-05 16:26:35 +0100

    chore: add category_slug to the Category model

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
+    category_slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.category_name
